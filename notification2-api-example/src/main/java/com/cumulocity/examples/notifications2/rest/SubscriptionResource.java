6bf8cc0f8542a505b17f77273a1b5fedf89c7fe6
Jakub Sobolewski <jakub.sobolewski@softwareag.com>
2025-05-26 16:56:08 +0200

    [DM-4600] - Notifications2Api usage example

--- notification2-api-example/src/main/java/com/cumulocity/examples/notifications2/rest/SubscriptionResource.java
+++ notification2-api-example/src/main/java/com/cumulocity/examples/notifications2/rest/SubscriptionResource.java
@@ -1,18 +1,14 @@
-package notifications2.rest;
+package com.cumulocity.examples.notifications2.rest;
 
+import com.cumulocity.examples.notifications2.model.ApiSubscription;
+import com.cumulocity.examples.notifications2.service.SubscriptionService;
 import lombok.extern.slf4j.Slf4j;
-import notifications2.model.Subscription;
-import notifications2.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * This resource allows to dynamically subscribe/unsubscribe notification topics.
- * It's not required to have this to use Notifications2Api - it's just a part of test microservice logic.
- */
 @Slf4j
 @RestController
 public class SubscriptionResource {
@@ -26,8 +22,8 @@ public class SubscriptionResource {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseBody
-    public ResponseEntity<Void> subscribe(@RequestBody Subscription subscription) {
-        subscriptionService.subscribe(subscription);
+    public ResponseEntity<Void> subscribe(@RequestBody ApiSubscription apiSubscription) {
+        subscriptionService.subscribe(apiSubscription);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
@@ -39,8 +35,20 @@ public class SubscriptionResource {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseBody
-    public ResponseEntity<Void> unsubscribe(@RequestBody Subscription subscription) {
-        subscriptionService.unsubscribe(subscription);
+    public ResponseEntity<Void> unsubscribe(@RequestBody ApiSubscription apiSubscription) {
+        subscriptionService.unsubscribe(apiSubscription);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @RequestMapping(
+            path = "/delete",
+            method = RequestMethod.DELETE,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public ResponseEntity<Void> delete(@RequestBody ApiSubscription apiSubscription) {
+        subscriptionService.delete(apiSubscription);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
