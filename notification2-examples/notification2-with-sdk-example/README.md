7c7e509e6692467ff2e4c7768e634094252c3fcd
Jakub Sobolewski <jakub.sobolewski@softwareag.com>
2025-06-05 09:55:33 +0200

    [DM-4600] - example of notifications2 SDK

---
+++ notification2-examples/notification2-with-sdk-example/README.md
@@ -0,0 +1,5 @@
+This module showcases how to use Notifications2Api from C8Y Java SDK library.
+
+This is a simple microservice with one rest controller that allows to subscribe/unsubscribe to/from different targets.
+
+Example listener is only printing incoming messages.
---
+++ notification2-examples/notification2-with-sdk-example/src/main/java/com/cumulocity/examples/notifications2/Main.java
@@ -0,0 +1,11 @@
+package com.cumulocity.examples.notifications2;
+
+import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
+import org.springframework.boot.SpringApplication;
+
+@MicroserviceApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
