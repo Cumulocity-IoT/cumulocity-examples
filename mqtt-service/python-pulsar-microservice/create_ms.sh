45bddfd90f04cf9fd0e5477f969eb1e437eaee91
Scott Mitchell <46353356+scmi-c8y@users.noreply.github.com>
2026-03-24 17:57:39 +0000

    MTM-66295 Python Pulsar microservice for MQTT (#170)
    Co-authored-by: Marco Stoffel <marco.stoffel@cumulocity.com>

---
+++ mqtt-service/python-pulsar-microservice/create_ms.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Using the C8Y CLI to create the microservice application with required roles
+
+c8y applications create --key pulsar-python-mapper-ms --name pulsar-python-mapper-ms --type MICROSERVICE  --data 'requiredRoles=["ROLE_MQTT_SERVICE_MESSAGING_TOPICS_READ","ROLE_MQTT_SERVICE_MESSAGING_TOPICS_UPDATE",
+    "ROLE_INVENTORY_CREATE",
+    "ROLE_IDENTITY_READ",
+    "ROLE_IDENTITY_ADMIN",
+    "ROLE_MEASUREMENT_ADMIN"
+  ]'
